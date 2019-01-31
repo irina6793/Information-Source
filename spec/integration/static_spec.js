594971c25c3caf512851b227000a4d96686d1832
Irina <irina6793@yahoo.com>
2019-01-30 22:24:15 -0500

    first

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -3,21 +3,23 @@ const server = require("../../src/server");
 const base = "http://localhost:3000/";
 
 describe("routes : static", () => {
-
-//#1
   describe("GET /", () => {
-
-//#2
-    it("should return status code 200", (done) => {
-
-//#3
+    it("should return status code 200 and have 'Welcome to Blocipedia' in the body of the response", (done) => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
-
-//#4
+        expect(body).toContain("Welcome to Blocipedia");
         done();
       });
     });
+   });
 
-  });
+describe("GET /about", () => {
+     it("should return status code 200 and have 'Interesting Info' in the middle of the response", done => {
+        request.get(base, (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toContain("Interesting Info");
+                done();
+     })
+   })
+  })
 });
--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,4 +1,11 @@
 require("dotenv").config();
+const path = require("path");
+const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
+const session = require("express-session");
+const flash = require("express-flash");
+const passportConfig = require("./passport-config");
 
 module.exports = {
   init(){
