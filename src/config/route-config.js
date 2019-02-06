4e0fcf34b6f1054db878ab3b702a34fdc99d7259
Irina <irina6793@yahoo.com>
2019-02-06 14:12:57 -0500

    updates

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,6 +1,9 @@
+const staticRoutes = require("../routes/static");
+const topicRoutes = require("../routes/topics");
+
+app.use(staticRoutes);
+app.use(topicRoutes);
 module.exports = {
   init(app){
-    const staticRoutes = require("../routes/static");
-    app.use(staticRoutes);
   }
 }
