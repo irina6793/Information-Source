b009eadc22e743501950bc62228a472fb367e490
Irina <irina6793@yahoo.com>
2019-02-09 13:47:53 -0500

    updates

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const validation = require("./validation");
 
 router.get("/user/signup", userController.signup);
+router.post("/users", validation.validateUsers, userController.create);
 
 
 
