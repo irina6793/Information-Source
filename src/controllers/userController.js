3e662c026ecc89066b6a465e461c35f68056e1ca
Irina <irina6793@yahoo.com>
2019-02-09 19:15:19 -0500

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,14 @@
 const userQueries = require("../db/queries.users.js");
 const passport = require("passport");
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'test@example.com',
+  from: 'test@example.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
 
 module.exports = {
   signup(req, res, next){
@@ -19,6 +28,7 @@ module.exports = {
 
   passport.authenticate("local")(req, res, () => {
     req.flash("notice", "You've successfully signed in!");
+    sgMail.send(msg);
     res.redirect("/");
   })
 }
@@ -52,5 +62,5 @@ module.exports = {
         res.render("users/show", {...result});
       }
   });
+ }
 }
-   }
