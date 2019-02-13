3241952a9fa147eb02676dc32476c091d5dce7e9
Irina <irina6793@yahoo.com>
2019-02-13 18:09:26 -0500

    update

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,8 +4,8 @@ const userController = require("../controllers/userController");
 
 router.get("/users/signup", userController.signup);
 router.post("/users", userController.create);
-router.get("/users/sign_in", userController.signIn);
-router.post("/users/sign_in", userController.signIn);
+router.get("/user/sign_in", userController.signIn);
+router.post("/user/sign_in", userController.signIn);
 router.get("/users/sign_out", userController.signOut);
 
 
