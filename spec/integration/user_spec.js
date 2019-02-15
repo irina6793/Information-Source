9534f03206c37ce72f258101907747416ee313f6
Irina <irina6793@yahoo.com>
2019-02-15 17:35:13 -0500

    updates

--- spec/integration/user_spec.js
+++ spec/integration/user_spec.js
@@ -81,7 +81,7 @@ describe("GET /user/sign_in", () => {
    it("should render a view with a sign in form", (done) => {
      request.get(`${base}sign_in`, (err, res, body) => {
        expect(err).toBeNull();
-       expect(body).toContain("Sign in");
+       expect(body).toContain("Sign In");
        done();
      });
    });
---
+++ src/db/models/wiki.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Wiki = sequelize.define('Wiki', {
+    title: DataTypes.STRING,
+    body: DataTypes.STRING,
+    private: DataTypes.BOOLEAN
+});
+   Wiki.associate = function(models) {
+     // associations can be defined here
+      Wiki.belongsTo(models.User {
+          foreignKey: "userId",
+          onDelete: "CASCADE",
+     });
+  };
+  
+  return Wiki;
+};
