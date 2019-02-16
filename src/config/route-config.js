00cacaa5ec0f9b9bac2b003d8e828a5d353ec146
Irina <irina6793@yahoo.com>
2019-02-15 21:54:13 -0500

    update

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,8 +3,11 @@ module.exports = {
 
      const staticRoutes = require("../routes/static");
      const userRoutes = require("../routes/user");
+     const wikiRoutes = require("../routes/wiki");
 
     app.use(staticRoutes);
     app.use(userRoutes);
+    app.use(wikiRoutes);
+
   }
 }
