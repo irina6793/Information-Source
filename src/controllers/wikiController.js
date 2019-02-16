20922acebb028c39f8274340a790f0954245e942
Irina <irina6793@yahoo.com>
2019-02-16 18:46:23 -0500

    update

--- src/controllers/wikiController.js
+++ src/controllers/wikiController.js
@@ -1,71 +1,87 @@
 const wikiQueries = require("../db/queries.wiki.js");
+const Authorizer = require("../policies/topic");
 
 module.exports = {
-
-    index(req, res, next) {
-      wikiQueries.getAllWikis((err, wiki) => {
-        if(err){
-          console.log(err);
-          res.redirect(500, "static/index");
-        } else {
-      res.render("wiki/index", {wiki});
-    }
-  })
-},
-   new(req, res, next){
-      res.render("wiki/new");
-  },
-   create(req, res, next){
-      let newWiki= {
-           title: req.body.title,
-           description: req.body.description
-         };
-         wikiQueries.addWiki(newWiki, (err, wiki) => {
-           if(err){
-             res.redirect(500, "/wiki/new");
-           } else {
-             res.redirect(303, `/wiki/${wiki.id}`);
-           }
-         })
-       },
-
-    show(req, res, next){
-        wikiQueries.getWiki(req.params.id, (err, wiki) => {
-        if(err || wiki == null){
-           res.redirect(404, "/");
-       } else {
-           res.render("wiki/show", {wiki});
-        }
-       })
-     },
-
-     destroy(req, res, next){
-       wikiQueries.deleteWiki(req.params.id, (err, wiki) => {
-         if(err){
-           res.redirect(500, `/wiki/${wiki.id}`)
-         } else {
-           res.redirect(303, "/wiki")
-         }
-       });
-     },
-
-     edit(req, res, next){
-       wikiQueries.getWiki(req.params.id, (err, wiki) => {
-         if(err || wiki == null){
-           res.redirect(404, "/");
-         } else {
-           res.render("wiki/edit", {wiki});
-        }
-      })
-     },
-
-     update(req, res, next){
-       wikiQueries.updateWiki(req.params.id, req.body, (err, wiki) => {
-         if(err || wiki == null){
-           res.redirect(404, `/wiki/${req.params.id}/edit`);
-         } else {
-           res.redirect(`/wiki/${wiki.id}`);
-         }
-       });
-     }
+  index(req, res, next){
+    wikiQueries.getAllWikis((err, wikis) => {
+    if(err){
+    res.redirect(500, "static/index");
+  } else {
+    res.render("wikis/index", {wikis});
    }
+  })
+ },
+ new(req, res, next){
+const authorized = new Authorizer(req.user).new();
+if(authorized) {
+   res.render("wikis/new");
+ } else {
+   req.flash("notice", "You are not authorized to do that.");
+   res.redirect("/wikis");
+ }
+},
+  create(req, res, next){
+  const authorized = new Authorizer(req.user).create();
+  if(authorized) {
+    let newWiki = {
+      title: req.body.title,
+      description: req.body.description
+  };
+   wikiQueries.addWiki(newWiki, (err, wiki) => {
+    if(err){
+      res.redirect(500, "/wikis/new");
+    } else {
+      res.redirect(303, `/wikis/${wiki.id}`);
+    }
+  });
+} else {
+  req.flash("notice", "You are not authorized to do that.");
+  res.redirect(303, "/wikis");
+ }
+},
+
+ show(req, res, next){
+   wikiQueries.getWiki(req.params.id, (err, wiki) => {
+   if(err || wiki == null){
+      res.redirect(404, "/");
+  } else {
+      res.render("wikis/show", {wiki});
+   }
+  });
+},
+edit(req, res, next){
+    wikiQueries.getWiki(req.params.id, (err, wiki) => {
+    if(err || wiki == null){
+      res.redirect(404, "/");
+    } else {
+   const authorized = new Authorizer(req.user, wiki).edit();
+   if(authorized){
+      res.render("wikis/edit", {wiki});
+    } else {
+      req.flash("You are not authorized to do that.")
+      res.redirect(`/wikis/${req.params.id}`)
+    }
+   }
+ });
+},
+update(req, res, next){
+// #1
+   topicQueries.updateTopic(req, req.body, (err, topic) => {
+   if(err || topic == null){
+      res.redirect(404, `/topics/${req.params.id}/edit`);
+    } else {
+      res.redirect(`/topics/${req.params.id}`);
+    }
+  });
+},
+destroy(req, res, next){
+// #1
+  topicQueries.deleteTopic(req, (err, topic) => {
+    if(err){
+      res.redirect(500, `/topics/${req.params.id}`)
+    } else {
+      res.redirect(303, "/topics")
+    }
+  });
+ }
+}
