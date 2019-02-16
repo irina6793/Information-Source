756ce5d9682082d401f099401cbb1e61f25dcc50
Irina <irina6793@yahoo.com>
2019-02-15 21:01:07 -0500

    update

--- src/db/models/wiki.js
+++ src/db/models/wiki.js
@@ -1,17 +1,30 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Wiki = sequelize.define('Wiki', {
-    title: DataTypes.STRING,
-    body: DataTypes.STRING,
-    private: DataTypes.BOOLEAN
-});
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    private: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    userId: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+       },
+  });
    Wiki.associate = function(models) {
      // associations can be defined here
-      Wiki.belongsTo(models.User {
+      Wiki.belongsTo(models.User, {
           foreignKey: "userId",
           onDelete: "CASCADE",
      });
   };
-  
+
   return Wiki;
 };
