2c04e8850cd9dc38ec6437f75c961826a439706e
Irina <irina6793@yahoo.com>
2019-02-18 19:23:03 -0500

    update

--- src/routes/wiki.js
+++ src/routes/wiki.js
@@ -5,7 +5,7 @@ const validation = require("./validation")
 
 router.get("/wikis", wikiController.index);
 router.get("/wikis/new", wikiController.new);
-router.get("/wikis/create", wikiController.create);
+router.post("/wikis/create", wikiController.create);
 router.get('/wikis/:id', wikiController.show);
 router.get("/wikis/:id/edit", wikiController.edit);
 router.post('/wikis/:id/update',  wikiController.update);
